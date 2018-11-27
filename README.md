eb571d86c1d26726c594b812c7a8b447f6457e67
cpeggg <594076223@qq.com>
2018-11-26 20:20:45 -0800

    hook other syscalls

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Fileauditor
 A Linux file audition-related syscall hook. Altered from X86, getting tested on Ubuntu 16.04 x64.
 ## TODO
-1. Hook the [syscalls related to fs](http://linasm.sourceforge.net/docs/syscalls/filesystem.php). Simplify the most sensitive syscalls in file audition which is included in "File operations", "Directory operations", "Link operations","Basic file attributes" and "Read/Write"
+1. Hook the [syscalls related to fs](http://linasm.sourceforge.net/docs/syscalls/filesystem.php). Simplify the most sensitive syscalls in file audition and [Control Flow Integrity](https://www.cc.gatech.edu/~hhu86/papers/ucfi.pdf)
 2. Hook execve (No.59)
 
 In a nutshell, the following syscalls are hooked:
@@ -12,12 +12,15 @@ WRITE 1 `ssize_t write(int fd, const void *buf, size_t count);`
 
 OPEN 2 `int open(const char *pathname, int flags, mode_t mode);` (mode is optional)
 
-CLOSE 3 `int close(int fd);`
+MMAP 9 `void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);`
 
-LSEEK 8 `off_t lseek(int fd, off_t offset, int whence);`
+MPROTECT 10 `int mprotect(void *addr, size_t len, int prot);`
 
 EXECVE 59 `int execve(const char *filename, char *const argv[], char *const envp[]`
 
 CREAT 85 `int creat(const char *pathname, mode_t mode);`
 
+REMAP\_FILE\_PAGES 216 `int remap_file_pages(void *addr, size_t size, int prot, size_t pgoff, int flags);`
+
 OPENAT 257 `int openat(int dirfd, const char *pathname, int flags, mode_t mode);` (mode is optional)
+
--- sdthook.c
+++ sdthook.c
@@ -14,7 +14,7 @@
 #include <linux/unistd.h>
 #include <linux/slab.h>
 #define MAX_LENGTH 256
-#define BUFFER_SIZE 0x1000
+#define BUFFER_INIT_SIZE 0x10000
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("cpegg");
@@ -24,7 +24,6 @@ MODULE_VERSION("0.01");
 void netlink_release(void);
 void netlink_init(void);
 int AuditOpen(const char *pathname, int flags, int ret);
-void* buf;//for univeral use
 
 void *get_sys_call_table(void);
 #ifdef _X86_
@@ -39,54 +38,76 @@ unsigned long * sys_call_table=NULL;
 asmlinkage ssize_t (* orig_read)(int fd, void *buf, size_t count);
 asmlinkage ssize_t (* orig_write)(int fd, const void *buf, size_t count);
 asmlinkage int (* orig_open)(const char *pathname, int flags, mode_t mode);
-asmlinkage int (* orig_close)(int fd);
-asmlinkage off_t (* orig_lseek)(int fd, off_t offset, int whence);
+asmlinkage void* (* orig_mmap)(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+asmlinkage int (* orig_mprotect)(void *addr, size_t len, int prot);
 asmlinkage int (* orig_execve)(const char *filename, char *const argv[], char *const envp[]);
 asmlinkage int (* orig_creat)(const char *pathname, mode_t mode);
 asmlinkage int (* orig_openat)(int dirfd, const char *pathname, int flags, mode_t mode);
+asmlinkage int (* orig_remap_file_pages)(void *addr, size_t size, int prot, size_t pgoff, int flags);
+
+
 
 asmlinkage ssize_t hook_read(int fd, void *buf, size_t count){
-    printk(KERN_DEBUG "hook_read");
-
-    return orig_read(fd, buf, count);
+    ssize_t ret;
+    void *bufferSDTHook=kzalloc(count,GFP_ATOMIC);
+    ret = orig_read(fd, buf, count);
+    if (ret>=0)
+        copy_from_user(bufferSDTHook, buf, count); // to avoid copy \0 to userspace when ret < count
+    // To Apply analysis
+    // ...
+    kfree(bufferSDTHook);
+    return ret;
 }
 asmlinkage ssize_t hook_write(int fd, const void *buf, size_t count){
-    printk(KERN_DEBUG"hook_write");
+    void *bufferSDTHook=kzalloc(count,GFP_ATOMIC);
+    copy_from_user(bufferSDTHook, buf, count);
+    // Apply content analysis
+    // ...
+    kfree(bufferSDTHook);
     return orig_write(fd, buf, count);
 }
 asmlinkage long hook_open(const char *pathname, int flags, mode_t mode)
 {
 	long ret;
-    printk(KERN_DEBUG"hook_open");
+    void *bufferSDTHook=kzalloc(MAX_LENGTH,GFP_ATOMIC);
     if( pathname == NULL ) return -1;
+    
     // To secure from crash (SMAP protect)
-    memset(buf,0,BUFFER_SIZE);
-    copy_from_user(buf,pathname,MAX_LENGTH);
+    copy_from_user(bufferSDTHook,pathname,MAX_LENGTH);
 	ret = orig_open(pathname, flags, mode);
-  	AuditOpen(buf,flags,ret);
+  	AuditOpen(bufferSDTHook,flags,ret);
+    kfree(bufferSDTHook);
   	return ret; 
 }
-asmlinkage long hook_close(int fd){
-    printk(KERN_DEBUG"hook_close");
-    return orig_close(fd);
+asmlinkage void* hook_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset){
+    // Apply var analysis
+    return orig_mmap(addr, length, prot, flags, fd, offset);
 }
-asmlinkage off_t hook_lseek(int fd, off_t offset, int whence){
-    printk(KERN_DEBUG"hook_lseek");
-    return orig_lseek(fd, offset, whence);
+asmlinkage long hook_mprotect(void *addr, size_t len, int prot){
+    // Apply var analysis
+    return orig_mprotect(addr, len, prot);
 }
 asmlinkage long hook_execve(const char *filename, char *const argv[], char *const envp[]){
-    printk(KERN_DEBUG"hook_execve");
+    void *bufferSDTHook=kzalloc(MAX_LENGTH,GFP_ATOMIC);
+    copy_from_user(bufferSDTHook,filename,MAX_LENGTH);
+    kfree(bufferSDTHook);
     return orig_execve(filename, argv, envp);
 }
 asmlinkage long hook_creat(const char *pathname, mode_t mode){
-    printk(KERN_DEBUG"hook_creat");
+    void *bufferSDTHook=kzalloc(MAX_LENGTH,GFP_ATOMIC);
+    copy_from_user(bufferSDTHook,pathname,MAX_LENGTH);
+    kfree(bufferSDTHook);
     return orig_creat(pathname, mode);
 }
 asmlinkage long hook_openat(int dirfd, const char *pathname, int flags, mode_t mode){
-    printk(KERN_DEBUG"hook_openat");
+    void *bufferSDTHook=kzalloc(MAX_LENGTH,GFP_ATOMIC);
+    copy_from_user(bufferSDTHook,pathname,MAX_LENGTH);
+    kfree(bufferSDTHook);
     return orig_openat(dirfd, pathname, flags, mode);
 }
-  
+asmlinkage long hook_remap_file_pages(void *addr, size_t size, int prot, size_t pgoff, int flags){
+    return orig_remap_file_pages(addr, size, prot, pgoff, flags);
+}
 
 static int __init audit_init(void)
 {
@@ -94,11 +115,9 @@ static int __init audit_init(void)
 	unsigned int orig_cr0;
 #else
     unsigned long orig_cr0;
-#endif
+#endif    
     printk("Audit Module Loading...\n");
     
-    buf=kzalloc(BUFFER_SIZE,GFP_KERNEL);
-
     orig_cr0 = clear_and_return_cr0();
 
 	sys_call_table = get_sys_call_table();
@@ -112,12 +131,12 @@ static int __init audit_init(void)
     //Hook Sys Call Write
     orig_write = (void*)sys_call_table[__NR_write];
     sys_call_table[__NR_write] = (unsigned long)&hook_write;
-    //Hook Sys Call Close
-    orig_close = (void*)sys_call_table[__NR_close];
-    sys_call_table[__NR_close] = (unsigned long)&hook_close;
-    //Hook Sys Call Lseek
-    orig_lseek = (void*)sys_call_table[__NR_lseek];
-    sys_call_table[__NR_lseek] = (unsigned long)&hook_lseek;
+    //Hook Sys Call Mmap 
+    orig_mmap = (void*)sys_call_table[__NR_mmap];
+    sys_call_table[__NR_mmap] = (unsigned long)&hook_mmap;
+    //Hook Sys Call Mprotect
+    orig_mprotect = (void*)sys_call_table[__NR_mprotect];
+    sys_call_table[__NR_mprotect] = (unsigned long)&hook_mprotect;
     //Hook Sys Call Execve
     orig_execve = (void*)sys_call_table[__NR_execve];
     sys_call_table[__NR_execve] = (unsigned long)&hook_execve;
@@ -127,6 +146,9 @@ static int __init audit_init(void)
     //Hook Sys Call Openat
     orig_openat = (void*)sys_call_table[__NR_openat];
     sys_call_table[__NR_openat] = (unsigned long)&hook_openat;
+    //Hook Sys Call Remap_file_pages
+    orig_remap_file_pages = (void*)sys_call_table[__NR_remap_file_pages];
+    sys_call_table[__NR_remap_file_pages] = (unsigned long)&hook_remap_file_pages;
 
     setback_cr0(orig_cr0);
 	
@@ -149,14 +171,14 @@ static void __exit audit_exit(void)
     sys_call_table[__NR_open] = (unsigned long)orig_open;
 	sys_call_table[__NR_read] = (unsigned long)orig_read;
     sys_call_table[__NR_write] = (unsigned long)orig_write;
-    sys_call_table[__NR_close] = (unsigned long)orig_close;
-    sys_call_table[__NR_lseek] = (unsigned long)orig_lseek;
+    sys_call_table[__NR_mmap] = (unsigned long)orig_mmap;
+    sys_call_table[__NR_mprotect] = (unsigned long)orig_mprotect;
     sys_call_table[__NR_execve] = (unsigned long)orig_execve;
     sys_call_table[__NR_creat] = (unsigned long)orig_creat;
     sys_call_table[__NR_openat] = (unsigned long)orig_openat;
+    sys_call_table[__NR_remap_file_pages] = (unsigned long)orig_remap_file_pages;
 
     setback_cr0(orig_cr0);
-    kfree(buf);
  	netlink_release();  	
     printk(KERN_INFO "Module exit.\n");
 }
